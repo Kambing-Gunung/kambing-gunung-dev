e7b45b120ecbe78fd3afd47b49016f836498de9a
David NTB <davidntb7000@gmail.com>
2026-07-27 04:11:46 +0700

    feat: setup tailwind css

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,16 +3,18 @@ import { Outlet } from "react-router";
 function MainLayout() {
   return (
     <>
-      <header>
-        <h2>Navbar</h2>
+      <header className="bg-red-500 p-4">
+        <h2 className="text-white text-2xl font-bold">
+          Navbar
+        </h2>
       </header>
 
-      <main>
+      <main className="p-8">
         <Outlet />
       </main>
 
-      <footer>
-        <p>Footer</p>
+      <footer className="bg-slate-900 text-white p-4">
+        Footer
       </footer>
     </>
   );
